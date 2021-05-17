a084a68938d61ee2d5627607c34e0a051c79f37e
1dami <1dami@HZ341336.TartangaLH.eus>
2021-05-17 09:09:24 +0200

    new commit

--- Git project EDE/src/project/Project.java
+++ Git project EDE/src/project/Project.java
@@ -5,8 +5,6 @@ public class Project {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is simple code for git");
-		// change in eclipse
-		// a simple change
 	}
 
 }
